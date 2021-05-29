c4c6db77bb746b47fd978d2a22041b5df1783c54
nurhamidan <agungnurhamidan@gmail.com>
2021-05-29 13:01:17 +0700

    Menambahkan Identitas.

--- app/src/main/java/com/nurhamidan/quiz10118017/MainActivity.java
+++ app/src/main/java/com/nurhamidan/quiz10118017/MainActivity.java
@@ -4,7 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 /*
-
+Tanggal pengerjaan: 29 Mei 2021
+Nim : 10118017
+Nama : Agung Nurhamidan
+Kelas : IF-1
  */
 
 public class MainActivity extends AppCompatActivity {
